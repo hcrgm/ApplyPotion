5aef693b4232ec6e46144039912fc42a5472524e
hcrgm <hcrgm@qq.com>
2020-02-28 09:14:19 +0800

    Fix various bugs

--- src/main/java/com/aplugin/potionondifferentworld/DifferentWorldPotionApplication.java
+++ src/main/java/com/aplugin/potionondifferentworld/DifferentWorldPotionApplication.java
@@ -2,31 +2,27 @@ package com.aplugin.potionondifferentworld;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPotionEffectEvent;
-import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-
-import java.util.Collection;
 
 public final class DifferentWorldPotionApplication extends JavaPlugin implements Listener {
 
     private boolean debug;
+    private String messagePrefix;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         debug = getConfig().getBoolean("debug");
+        messagePrefix = ChatColor.GRAY + "[" + ChatColor.GOLD + getName() + ChatColor.GRAY + "]" + ChatColor.RESET + " ";
         getCommand("applypotion").setExecutor(this);
         applyToAllPlayers();
         getServer().getPluginManager().registerEvents(this, this);
@@ -34,6 +30,7 @@ public final class DifferentWorldPotionApplication extends JavaPlugin implements
 
     @Override
     public void onDisable() {
+        messagePrefix = null;
         if (getConfig().getBoolean("clear")) {
             for (Player p : Bukkit.getOnlinePlayers()) {
                 PotionEffectType desiredEffect = getPotionEffectTypeByWorld(p.getWorld().getName());
@@ -59,25 +56,32 @@ public final class DifferentWorldPotionApplication extends JavaPlugin implements
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!sender.isOp()) return true;
+        if (!sender.isOp()) {
+            sender.sendMessage(messagePrefix + ChatColor.RED + "您没有权限执行此命令");
+            return true;
+        }
         if (args.length > 0) {
             if (args[0].equalsIgnoreCase("reload")) {
                 reloadConfig();
                 debug = getConfig().getBoolean("debug");
                 applyToAllPlayers();
-                sender.sendMessage(ChatColor.GREEN + "已重载!");
+                sender.sendMessage(messagePrefix + ChatColor.GREEN + "已重载!");
+                return true;
             }
         }
+        sender.sendMessage(messagePrefix + ChatColor.GOLD + "重载配置:/" + label + " reload");
         return true;
     }
 
     @EventHandler
     public void onChangeWorld(PlayerChangedWorldEvent e) {
-        applyPotionEffect(e.getPlayer());
-    }
-
-    @EventHandler
-    public void onPlayerRespawn(PlayerRespawnEvent e) {
+        Player p = e.getPlayer();
+        PotionEffectType oldEffect = getPotionEffectTypeByWorld(e.getFrom().getName());
+        if (oldEffect != null) {
+            if (p.hasPotionEffect(oldEffect)) {
+                p.removePotionEffect(oldEffect);
+            }
+        }
         applyPotionEffect(e.getPlayer());
     }
 
@@ -86,68 +90,46 @@ public final class DifferentWorldPotionApplication extends JavaPlugin implements
         applyPotionEffect(e.getPlayer());
     }
 
-    /*@EventHandler
-    public void onDrinkMilk(PlayerBucketEmptyEvent e) {
-        if (e.getBucket() == Material.MILK_BUCKET) {
-            e.setCancelled(true);
-        }
-    }*/
-
     @EventHandler
     public void onChangePotionEffect(EntityPotionEffectEvent e) {
         if (!(e.getEntity() instanceof Player)) return;
         Player p = (Player) e.getEntity();
+        if (e.getCause() == EntityPotionEffectEvent.Cause.COMMAND || e.getCause() == EntityPotionEffectEvent.Cause.PLUGIN)
+            return;
         switch (e.getCause()) {
             case MILK:
-                Bukkit.getScheduler().runTaskLater(this, () -> {
-                    applyPotionEffect(p); // 等待效果清除, 延时设置
-                }, 2L);
+                Bukkit.getScheduler().runTaskLater(this, () -> applyPotionEffect(p), 1L); // 延时设置, 等待效果清除
                 break;
             case EXPIRATION:
                 if (!getConfig().getBoolean("reapply")) break;
                 PotionEffectType desiredEffect = getPotionEffectTypeByWorld(p.getWorld().getName());
                 if (desiredEffect == null) break;
-                if (e.getOldEffect().getType() == desiredEffect) {
-                    applyPotionEffect(p);
+                if (e.getOldEffect() != null) {
+                    if (e.getOldEffect().getType() == desiredEffect) {
+                        e.setCancelled(true);
+                        applyPotionEffect(p, true);
+                    }
                 }
                 break;
+            default:
+                if (getConfig().getConfigurationSection("worldList").contains(p.getWorld().getName())) {
+                    applyPotionEffect(p);
+                }
         }
     }
 
-    /*
-     此代码可能产生bug, 舍弃
-     Player p = (Player) e.getEntity();
-            PotionEffectType desiredEffect = getPotionEffectTypeByWorld(p.getWorld().getName());
-            if (desiredEffect == null) return;
-            switch (e.getAction()) {
-                case CHANGED:
-                    if (!p.hasPotionEffect(desiredEffect)) {
-                        applyPotionEffect(p);;
-                    }
-                    break;
-                case CLEARED:
-                    applyPotionEffect(p);
-                    break;
-                case REMOVED:
-                    if (e.getOldEffect().getType() == desiredEffect) {
-                        applyPotionEffect(p);
-                    }
-                    break;
-            }
-     */
-
     private void applyPotionEffect(Player p) {
+        applyPotionEffect(p, false);
+    }
+
+    private void applyPotionEffect(Player p, boolean force) {
         PotionEffectType potionEffectType = getPotionEffectTypeByWorld(p.getWorld().getName());
         if (potionEffectType == null) {
             return;
         }
         int duration = getConfig().getConfigurationSection("worldList").getConfigurationSection(p.getWorld().getName()).getInt("duration") * 20; // 转化为tick
         int amplifier = getConfig().getConfigurationSection("worldList").getConfigurationSection(p.getWorld().getName()).getInt("amplifier");
-        Collection<PotionEffect> playerEffects = p.getActivePotionEffects();
-        for (PotionEffect effect : playerEffects) {
-            p.removePotionEffect(effect.getType());
-        }
-        p.addPotionEffect(potionEffectType.createEffect(duration, amplifier));
+        p.addPotionEffect(potionEffectType.createEffect(duration, amplifier), force);
     }
 
     private PotionEffectType getPotionEffectTypeByWorld(String world) {
@@ -155,6 +137,8 @@ public final class DifferentWorldPotionApplication extends JavaPlugin implements
             debug("配置worldList中不含指定世界" + world + "!");
             return null;
         }
+        if (!getConfig().getConfigurationSection("worldList").getConfigurationSection(world).contains("effect"))
+            return null;
         String potionEffectName = getConfig().getConfigurationSection("worldList").getConfigurationSection(world).getString("effect");
         PotionEffectType potionEffectType = PotionEffectType.getByName(potionEffectName);
         if (potionEffectType == null) {
